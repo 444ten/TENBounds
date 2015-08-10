f8b7f0db7d65da2f3f9b05c1fef011b3bd3b4084
444ten <444ten@Chas.local>
2015-08-10 09:33:06 +0300

    Initial Commit

---
+++ TENBounds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TENBounds
+//
+//  Created by 444ten on 8/10/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
